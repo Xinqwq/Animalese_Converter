1091a9d81b6ff013a618cf6c8237ba2f73022c7e
Kelsi Zhang <xz15@student.unimelb.edu.au>
2025-09-27 13:16:25 +1000

    add exit button to enhance the program

---
+++ Assets/ChineseGibberish/Scripts/ExitButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit(); // Exit Program
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
